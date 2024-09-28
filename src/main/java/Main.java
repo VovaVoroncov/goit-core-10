c16698973f170e8628695bcbfcb829ab653d9652
VovaVoroncov <gamesvova803@gmail.com>
2024-09-28 14:00:54 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import task1.PhoneNumberValidator;
+import task2.UserFileProcessor;
+import task3.WordFrequencyCounter;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String phoneNumbersPath = "src/main/resources/phoneNumbers.txt";
+        File phoneNumbersFile = new File(phoneNumbersPath);
+        PhoneNumberValidator.printValidNumbersFromFile(phoneNumbersFile);
+        System.out.println("=".repeat(100));
+
+        String wordsPath = "src/main/resources/words.txt";
+        File wordsFile = new File(wordsPath);
+        WordFrequencyCounter.displayWordFrequenciesSorted(wordsFile);
+        System.out.println("=".repeat(100));
+
+        String usersPath = "src/main/resources/users.txt";
+        File usersFile = new File(usersPath);
+        UserFileProcessor.createAndWriteJsonFile(usersFile);
+    }
+}
